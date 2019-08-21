1c595b6883205418cde4028d363a87bcc451e6f4
Ivan Fedulov <fedulov.ivan@gmail.com>
2019-08-21 19:19:08 +0300

    added example with React.FC

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,27 @@ import { camelCase } from './utils'
 import ReactDOMServer from 'react-dom/server';
 import ReactDOM from 'react-dom';
 import Counter from './react/Counter';
+import Foo from './react/Foo';
 import React from 'react';
 
-const componentInstance = React.createElement(Counter, { startFrom: 10 })
+const IS_BROWSER = typeof window !== 'undefined';
 
 // invoke a simple function
 console.log(camelCase('foo'));
 
-if (typeof window !== 'undefined') {
+if (IS_BROWSER) {
     // render react component in browser
     const target = window.document.getElementById('react-target');
-    ReactDOM.render(componentInstance, target);
-    if (target) target.style.display = 'block';
+    if (target) {
+        ReactDOM.render(
+            [<Counter startFrom={10} key="1" />, <Foo value="demo value" key="2" />],
+            target
+        );
+        target.style.display = 'block';
+    }
 } else {
-    // render react component into string on server
-    const string = ReactDOMServer.renderToString(componentInstance);
+    // render react component into string
+    const counterComponent = React.createElement(Counter, { startFrom: 10 })
+    const string = ReactDOMServer.renderToString(counterComponent);
     console.log(string);
 }
--- src/react/Counter.tsx
+++ src/react/Counter.tsx
@@ -2,23 +2,22 @@ import React, { useState } from 'react';
 import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 
-interface CounterProps {
+interface Props {
     startFrom: number;
+    foo?: string;
 }
 
-export default function Counter(props: CounterProps): React.ReactElement {
+export default function Counter(props: Props): React.ReactElement {
     const [counter, setCounter] = useState(props.startFrom);
     function handleClick(event: React.MouseEvent<HTMLButtonElement>): void {
         setCounter(counter + (event.currentTarget.id === 'inc' ? 1 : -1));
     }
     function handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
-        setCounter(parseInt(event.currentTarget.value, 10));
+        const { value } = event.currentTarget;
+        setCounter(value ? parseInt(value, 10) : 0);
     }
     return (
         <div>
-            {/* <input type="text" value={counter} readOnly />
-            <button id="inc" onClick={handleClick}>Increase +</button>
-            <button id="dec" onClick={handleClick}>Decrease -</button> */}
             <Button id="dec" onClick={handleClick}>Dec</Button>
             <Input type="number" value={counter} onChange={handleChange}></Input>
             <Button id="inc" onClick={handleClick}>Inc</Button>
